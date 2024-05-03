244f6ba2e3417c4e55f3bb341cfa50a9ed5b746e
Dolacinski <M.Dolacinski9736@student.leedsbeckett.ac.uk>
2024-05-04 00:10:41 +0100

    Nav Bar Implementation

--- app/src/main/java/com/example/productionproject/view/LoginActivity.kt
+++ app/src/main/java/com/example/productionproject/view/LoginActivity.kt
@@ -1,27 +1,41 @@
 package com.example.productionproject.view
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.EditText
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.example.productionproject.R
-import android.content.Intent
+import com.example.productionproject.viewmodel.MainActivity
 
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var videoView: VideoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = FirebaseAuth.getInstance()
+        videoView = findViewById(R.id.videoView)
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.loginvideo
+        videoView.setVideoURI(Uri.parse(videoPath))
+        videoView.setOnPreparedListener { mp ->
+            mp.isLooping = true
+            videoView.start()
+        }
+
 
         val emailEditText: EditText = findViewById(R.id.emailEditText)
         val passwordEditText: EditText = findViewById(R.id.passwordEditText)
         val loginButton: Button = findViewById(R.id.loginButton)
+        val registerButton: TextView = findViewById(R.id.registerButton)
 
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString().trim()
@@ -31,17 +45,22 @@ class LoginActivity : AppCompatActivity() {
                 auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
-                            // Log in success, update UI with the signed-in user's information
                             Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
-                            // Intent to navigate to another activity
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
                         } else {
-                            // If sign in fails, display a message to the user.
                             Toast.makeText(this, "Authentication failed: ${task.exception?.message}", Toast.LENGTH_LONG).show()
                         }
                     }
             } else {
                 Toast.makeText(this, "Email and password cannot be empty", Toast.LENGTH_LONG).show()
             }
+        }
+
+
+        registerButton.setOnClickListener {
+            // Navigate to RegisterActivity
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/example/productionproject/view/RegisterActivity.kt
+++ app/src/main/java/com/example/productionproject/view/RegisterActivity.kt
@@ -1,28 +1,39 @@
 package com.example.productionproject.view
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.example.productionproject.R
-
-
+import android.widget.VideoView
+import android.net.Uri
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var videoView: VideoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         auth = FirebaseAuth.getInstance()
 
+        videoView = findViewById(R.id.videoView)
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.loginvideo
+        videoView.setVideoURI(Uri.parse(videoPath))
+        videoView.setOnPreparedListener { mp ->
+            mp.isLooping = true
+            videoView.start()
+        }
+
+
         val emailEditText: EditText = findViewById(R.id.emailEditText)
         val confirmEmailEditText: EditText = findViewById(R.id.confirmEmailEditText)
         val passwordEditText: EditText = findViewById(R.id.passwordEditText)
         val confirmPasswordEditText: EditText = findViewById(R.id.confirmPasswordEditText)
-        val registerButton: Button = findViewById(R.id.registerButton)
+        val registerButton: TextView = findViewById(R.id.registerButton)
 
         registerButton.setOnClickListener {
             val email = emailEditText.text.toString().trim()
@@ -35,10 +46,12 @@ class RegisterActivity : AppCompatActivity() {
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
                             Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
-                            // Navigate to login or main activity
+                            startActivity(Intent(this, LoginActivity::class.java))  // Redirect back to login
+                            finish()
                         } else {
                             Toast.makeText(this, "Registration failed: ${task.exception?.message}", Toast.LENGTH_LONG).show()
                         }
+
                     }
             } else {
                 Toast.makeText(this, "Emails or passwords do not match or fields are empty", Toast.LENGTH_LONG).show()
