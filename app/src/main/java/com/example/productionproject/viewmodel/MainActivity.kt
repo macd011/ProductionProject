eb8906d75a54167a1483c168d7d291f9e47b5c9d
Maciej Dolacinski <maciejdolacinski8@gmail.com>
2024-07-06 04:39:26 +0100

    Training Diary Landing Page

--- app/src/main/java/com/example/productionproject/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/productionproject/viewmodel/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.productionproject.viewmodel
 
 import android.os.Bundle
+import android.widget.TextView
 import com.example.productionproject.R
 import com.example.productionproject.view.BaseActivity
 import com.google.android.material.navigation.NavigationView
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun getContentViewId(): Int = R.layout.activity_main
@@ -11,5 +15,9 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val currentDateTextView: TextView = findViewById(R.id.current_date)
+        val currentDate = SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.getDefault()).format(Date())
+        currentDateTextView.text = currentDate
     }
 }
